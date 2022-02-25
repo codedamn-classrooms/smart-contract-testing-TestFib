f8ef74b1d55a273e1d20483bbfd959b7fdc7b651
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-02-25 16:52:27 +0530

    Update fibTest.js

--- test/fibTest.js
+++ test/fibTest.js
@@ -1,1 +1,14 @@
 
+const Fibonacci = artifacts.require("Fibonacci");
+
+contract("Fibonacci", () => {
+  let fibonacci = null;
+  before(async () => {
+    fibonacci = await Fibonacci.deployed();
+  });
+
+  it("should calculate fibonacci sequence", async () => {
+    const result = await fibonacci.fib(10);
+    assert(result.toNumber() === 55);
+  });
+});
